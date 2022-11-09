14499fc29def2e0d9074b0394cb1f5865080b08a
Ayla P. Avellaneda <aylapa68@gmail.com>
2022-11-09 18:13:28 +0100

    initial commit

---
+++ src/main/java/es/aylait/bookingaux/BookingauxApplication.java
@@ -0,0 +1,13 @@
+package es.aylait.bookingaux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingauxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingauxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/aylait/bookingaux/BookingauxApplicationTests.java
@@ -0,0 +1,13 @@
+package es.aylait.bookingaux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingauxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
